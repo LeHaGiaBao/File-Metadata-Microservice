efa2eed3935b9d73cdabb09b38726d02863fac68
Le Ha Gia Bao <80085425+LeHaGiaBao@users.noreply.github.com>
2022-05-29 17:08:43 +0700

    Update file

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer')
+const bodyParser = require("body-parser");
+const path = require("path");
 
-var app = express();
+const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.route("/api/fileanalyse")
+  .post(multer().single("upfile"), (req, res) => {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+      });
+});
 
 
 const port = process.env.PORT || 3000;
